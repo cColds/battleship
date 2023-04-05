a88e94356f78d049c8c88c828315cfc93fa80d67
cColds <103373668+cColds@users.noreply.github.com>
2023-04-04 23:48:37 -0500

    Add random ship placement

---
+++ src/game-logic/player.js
@@ -0,0 +1,55 @@
+export default class Player {
+	constructor(gameboard, ...ships) {
+		this.gameboard = gameboard;
+		this.ships = ships;
+	}
+
+	static getRandomInt = (max) => Math.floor(Math.random() * max);
+
+	/* Randomize array in-place using Durstenfeld shuffle algorithm */
+	static shuffleArray(array) {
+		const newArray = [...array];
+		for (let i = array.length - 1; i > 0; i -= 1) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+		}
+
+		return newArray;
+	}
+
+	static getRandomOrientation = () =>
+		Player.getRandomInt(2) === 0 ? "horizontal" : "vertical";
+
+	static getRandomValidCoords = (validCoords) =>
+		validCoords[Player.getRandomInt(validCoords.length)];
+
+	placeAllShipsRandomly() {
+		const randomShips = Player.shuffleArray(this.ships);
+		randomShips.forEach((ship) => {
+			const randomOrientation = Player.getRandomOrientation();
+			const validCoords = [];
+
+			let row = 0,
+				col = 0;
+
+			while (row < 10 && col < 10) {
+				if (row === 9 && col === 9) {
+					const [randomX, randomY] =
+						Player.getRandomValidCoords(validCoords);
+					this.gameboard.placeShip(ship, [randomX, randomY]);
+				}
+				const isValidShipPlacement =
+					this.gameboard.areAllCellsAvailable(ship, [row, col]);
+				if (isValidShipPlacement) validCoords.push([row, col]);
+
+				if (randomOrientation === "horizontal") {
+					row = col === 9 ? row + 1 : row; // must set row before col because if col is 9, it'll reset to 0 and row won't increment
+					col = col !== 9 ? col + 1 : 0;
+				} else {
+					col = row === 9 ? col + 1 : col; // must set col before row because if row is 9, it'll reset to 0 and col won't increment
+					row = row !== 9 ? row + 1 : 0;
+				}
+			}
+		});
+	}
+}
---
+++ src/tests/player.test.js
@@ -0,0 +1,41 @@
+import Gameboard from "../game-logic/gameboard";
+import Player from "../game-logic/player";
+import Ship from "../game-logic/ship";
+
+let carrier, battleship, submarine, cruiser, destroyer, player, ai;
+
+beforeEach(() => {
+	carrier = new Ship(5);
+	battleship = new Ship(4);
+	cruiser = new Ship(3);
+	submarine = new Ship(3);
+	destroyer = new Ship(2);
+	player = new Player(
+		new Gameboard(),
+		carrier,
+		battleship,
+		submarine,
+		cruiser,
+		destroyer
+	);
+	ai = new Player(
+		new Gameboard(),
+		carrier,
+		battleship,
+		submarine,
+		cruiser,
+		destroyer
+	);
+});
+
+describe("player vs ai", () => {
+	it("should make move for ai after player attacks", () => {
+		// ai.receiveAttack([3, 2]);
+		// expect(player.receiveAttack(player.getRandomMove())).toBe(true);
+	});
+});
+
+// if turn is null, set it to player by default, else set it to the other player
+// if turn is ai, grab a random legal move (not out of bounds or hit already) and call player's board with receiveAttack(randomMove)
+// else, call ai's board with receiveAttack(yourMove)
+// if all ships have sunk, end game and set player who made the last move as the winner
