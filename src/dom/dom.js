3ccced11e490f8b19ef8f139942f49151fd0dbe3
cColds <103373668+cColds@users.noreply.github.com>
2023-04-12 17:52:10 -0500

    Refactor dom code

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -8,79 +8,6 @@ const Dom = (() => {
   const aiBoard = document.querySelector(".ai-board");
   const placeShipsBoard = document.querySelector(".place-ships-board");
 
-  // const shipToPlace = document.querySelector(".place-ship-hint");
-  // const rotateShip = document.querySelector(".rotate-ship");
-  // const randomizeShips = document.querySelector(".randomize-ships");
-  // const resetBoard = document.querySelector(".reset-board");
-  // const startGame = document.querySelector(".place-ships-start-game");
-
-  function generateGameboardCells(board) {
-    for (let i = 0; i < 100; i += 1) {
-      const cell = document.createElement("button");
-      cell.classList.add("cell");
-      board.appendChild(cell);
-    }
-  }
-
-  function generateGameboardCellCoords(board) {
-    let i = 0;
-    for (let row = 0; row < 10; row += 1) {
-      for (let col = 0; col < 10; col += 1) {
-        board.children[i].dataset.coords = `[${row}, ${col}]`;
-        i += 1;
-      }
-    }
-  }
-
-  function clearHighlightShip() {
-    const isValidShipHighlighted = document.querySelector(".valid");
-    const cellValidityName = isValidShipHighlighted ? "valid" : "invalid";
-    const cellsHighlighted = isValidShipHighlighted
-      ? document.querySelectorAll(".valid")
-      : document.querySelectorAll(".invalid");
-
-    cellsHighlighted.forEach((cell) => cell.classList.remove(cellValidityName));
-  }
-
-  function removeHighlightOnMouseLeave() {
-    placeShipsBoard.addEventListener("mouseleave", () => {
-      clearHighlightShip();
-    });
-  }
-
-  function highlightShip() {
-    [...placeShipsBoard.children].forEach((cell) =>
-      cell.addEventListener("mouseenter", (e) => {
-        clearHighlightShip();
-        const [currentShip] = player.shipsToPlace;
-
-        const [row, col] = JSON.parse(e.target.dataset.coords);
-        const isValidShipPlacement = player.gameboard.canPlaceShip(
-          currentShip,
-          [row, col]
-        );
-        const cellValidityName = isValidShipPlacement ? "valid" : "invalid";
-
-        for (let i = 0; i < currentShip.length; i += 1) {
-          let cellElement;
-          if (player.gameboard.isHorizontal()) {
-            if (player.gameboard.board[row][col + i] === null) {
-              cellElement = document.querySelector(
-                `.place-ships-board [data-coords="[${row}, ${col + i}]"]`
-              );
-              cellElement.classList.add(cellValidityName);
-            }
-          } else if (player.gameboard.board[row + i][col] === null) {
-            cellElement = document.querySelector(
-              `.place-ships-board [data-coords="[${row + i}, ${col}]"]`
-            );
-            cellElement.classList.add(cellValidityName);
-          }
-        }
-      })
-    );
-  }
-
   function initStartGameHomepage() {
     const startGameContainer = document.querySelector(".start-game-container");
     const startGameHomepage = document.querySelector(".homepage-start-game");
@@ -95,17 +22,94 @@ const Dom = (() => {
     });
   }
 
-  function initialize() {
-    initStartGameHomepage();
-    generateGameboardCells(playerBoard);
-    generateGameboardCells(aiBoard);
-    generateGameboardCells(placeShipsBoard);
+  function initPlaceShipsPage() {
+    // const shipToPlace = document.querySelector(".place-ship-hint");
+    // const rotateShip = document.querySelector(".rotate-ship");
+    // const randomizeShips = document.querySelector(".randomize-ships");
+    // const resetBoard = document.querySelector(".reset-board");
+    // const startGame = document.querySelector(".place-ships-start-game");
 
-    generateGameboardCellCoords(playerBoard);
-    generateGameboardCellCoords(aiBoard);
-    generateGameboardCellCoords(placeShipsBoard);
-    highlightShip();
-    removeHighlightOnMouseLeave();
+    function clearHighlightShip() {
+      const isValidShipHighlighted = document.querySelector(".valid");
+      const cellValidityName = isValidShipHighlighted ? "valid" : "invalid";
+      const cellsHighlighted = isValidShipHighlighted
+        ? document.querySelectorAll(".valid")
+        : document.querySelectorAll(".invalid");
+
+      cellsHighlighted.forEach((cell) =>
+        cell.classList.remove(cellValidityName)
+      );
+    }
+
+    function initHighlightShip() {
+      [...placeShipsBoard.children].forEach((cell) =>
+        cell.addEventListener("mouseenter", (e) => {
+          clearHighlightShip();
+          const [currentShip] = player.shipsToPlace;
+
+          const [row, col] = JSON.parse(e.target.dataset.coords);
+          const isValidShipPlacement = player.gameboard.canPlaceShip(
+            currentShip,
+            [row, col]
+          );
+          const cellValidityName = isValidShipPlacement ? "valid" : "invalid";
+
+          for (let i = 0; i < currentShip.length; i += 1) {
+            let [x, y] = [row, col];
+
+            if (player.gameboard.isHorizontal()) y = col + i;
+            else x = row + i;
+
+            const cellEl = document.querySelector(
+              `.place-ships-board [data-coords="[${x}, ${y}]"]`
+            );
+
+            if (cellEl) cellEl.classList.add(cellValidityName);
+          }
+        })
+      );
+    }
+
+    function initRemoveHighlightShip() {
+      placeShipsBoard.addEventListener("mouseleave", () => {
+        clearHighlightShip();
+      });
+    }
+    initHighlightShip();
+    initRemoveHighlightShip();
+  }
+
+  function initGameboardCells(board) {
+    for (let i = 0; i < 100; i += 1) {
+      const cell = document.createElement("button");
+      cell.classList.add("cell");
+      board.appendChild(cell);
+    }
+  }
+
+  function initGameboardCellCoords(board) {
+    let i = 0;
+    for (let row = 0; row < 10; row += 1) {
+      for (let col = 0; col < 10; col += 1) {
+        board.children[i].dataset.coords = `[${row}, ${col}]`;
+        i += 1;
+      }
+    }
+  }
+
+  // func placeSHip
+
+  function initialize() {
+    initGameboardCells(playerBoard);
+    initGameboardCells(aiBoard);
+    initGameboardCells(placeShipsBoard);
+
+    initGameboardCellCoords(playerBoard);
+    initGameboardCellCoords(aiBoard);
+    initGameboardCellCoords(placeShipsBoard);
+
+    initStartGameHomepage();
+    initPlaceShipsPage();
   }
 
   return { initialize };
