dd0a010705d6ab664a1eb69d37ab5ccc9fc0536a
cColds <103373668+cColds@users.noreply.github.com>
2023-04-12 00:14:01 -0500

    Add dynamic start game in homepage

---
+++ main.b93c41990e4c4c332575.js
@@ -0,0 +1,119 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/dom/dom.js":
+/*!************************!*\
+  !*** ./src/dom/dom.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _homepage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./homepage */ \"./src/dom/homepage.js\");\n/* harmony import */ var _homepage__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_homepage__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconst Dom = (() => {\n  const playerBoard = document.querySelector(\".player-board\");\n  const aiBoard = document.querySelector(\".ai-board\");\n  const playerPlaceShipsBoard = document.querySelector(\".place-ships-board\");\n\n  function generateGameboardCells(board) {\n    for (let i = 0; i < 100; i += 1) {\n      const cell = document.createElement(\"button\");\n      cell.classList.add(\"cell\");\n      board.appendChild(cell);\n    }\n  }\n\n  function initializeDom() {\n    generateGameboardCells(playerBoard);\n    generateGameboardCells(aiBoard);\n    generateGameboardCells(playerPlaceShipsBoard);\n  }\n\n  return { initializeDom };\n})();\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Dom);\n\n// make module pattern init dom load in this file\n// make module for home page\n// make module for place ship page\n// make module for player vs ai\n// make module helpers if needed\n\n\n//# sourceURL=webpack://battleship/./src/dom/dom.js?");
+
+/***/ }),
+
+/***/ "./src/dom/homepage.js":
+/*!*****************************!*\
+  !*** ./src/dom/homepage.js ***!
+  \*****************************/
+/***/ (() => {
+
+eval("const startGamePage = document.querySelector(\".start-game-container\");\nconst startGame = document.querySelector(\".homepage-start-game\");\nconst placeShipsPage = document.querySelector(\".place-ships-container\");\nconst placeShipsGameboard = document.querySelector(\".place-ships-gameboard\");\nstartGame.addEventListener(\"click\", () => {\n  startGamePage.classList.remove(\"active\");\n  placeShipsPage.classList.add(\"active\");\n  placeShipsGameboard.classList.add(\"active\");\n});\n\n\n//# sourceURL=webpack://battleship/./src/dom/homepage.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dom_dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dom/dom */ \"./src/dom/dom.js\");\n\n\n_dom_dom__WEBPACK_IMPORTED_MODULE_0__[\"default\"].initializeDom();\n\n\n//# sourceURL=webpack://battleship/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
